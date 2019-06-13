11e7bf87d3319bf32f09a79e62b0536f2fd979a4
Alexandro <alexandro.strauss@gmail.com>
2019-06-13 16:56:20 -0400

    working on HUD, which doesn't work and breaks everything

--- js/trackOneGeometry.js
+++ js/trackOneGeometry.js
@@ -1,12 +1,12 @@
 function trackOneGeometry () {
     var tower, tower2, boundingBox;
 
-    tower = new Physijs.BoxMesh(
+    tower = new THREE.Mesh(
         new THREE.BoxGeometry(90, 300, 90),
         new THREE.MeshBasicMaterial({ color: 0x0000ff }));
     scene.add(tower);
 
-    tower2 = new Physijs.BoxMesh(
+    tower2 = new THREE.Mesh(
         new THREE.BoxGeometry(90, 300, 90),
         new THREE.MeshBasicMaterial({ color: 0x0000ff }));
     scene.add(tower2);
@@ -19,24 +19,24 @@ function trackOneGeometry () {
     var frontstretchGeometry = new THREE.BoxGeometry(100, 100, 2050);
     var frontstretchMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
     var insideMaterial = new THREE.MeshBasicMaterial({ color: 0x111111 })
-    var frontstretch = new Physijs.BoxMesh(outsideStraightGeometry, insideMaterial);
-    var frontstretch2 = new Physijs.BoxMesh(frontstretchGeometry, frontstretchMaterial);
+    var frontstretch = new THREE.Mesh(outsideStraightGeometry, insideMaterial);
+    var frontstretch2 = new THREE.Mesh(frontstretchGeometry, frontstretchMaterial);
     scene.add(frontstretch);
     scene.add(frontstretch2);
     frontstretch.position.set(300, 50, 200);
     frontstretch2.position.set(-300, 50, 200);
 
-    var backstretch = new Physijs.BoxMesh(frontstretchGeometry, frontstretchMaterial);
+    var backstretch = new THREE.Mesh(frontstretchGeometry, frontstretchMaterial);
     scene.add(backstretch);
     backstretch.position.set(-1180, 50, 200);
-    var backstretch2 = new Physijs.BoxMesh(outsideStraightGeometry, insideMaterial);
+    var backstretch2 = new THREE.Mesh(outsideStraightGeometry, insideMaterial);
     scene.add(backstretch2);
     backstretch2.position.set(-1780, 50, 200);
 
     var cylinderGeometry = new THREE.CylinderGeometry(500, 500, 150, 64, 1, false, 0)
     var cylinderMaterial = new THREE.MeshBasicMaterial({ color: 0x111111 })
-    var cylinder = new Physijs.BoxMesh(cylinderGeometry, cylinderMaterial);
-    var cylinder2 = new Physijs.BoxMesh(cylinderGeometry, cylinderMaterial);
+    var cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+    var cylinder2 = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
     scene.add(cylinder)
     scene.add(cylinder2)
     cylinder.position.set(-740, 75, 1150)
@@ -44,19 +44,19 @@ function trackOneGeometry () {
 
     var sphereGeometry = new THREE.SphereGeometry(1000, 64, 64, 0, Math.PI, 0, Math.PI / 2)
     var sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff })
-    var sphere = new Physijs.SphereMesh(sphereGeometry, sphereMaterial)
+    var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
     sphere.material.side = THREE.BackSide;
     scene.add(sphere)
     sphere.position.set(-740, 0, 1350)
 
     var otherSphereGeometry = new THREE.SphereGeometry(1000, 64, 64, Math.PI, Math.PI, 0, Math.PI / 2)
-    var sphere2 = new Physijs.SphereMesh(otherSphereGeometry, sphereMaterial)
+    var sphere2 = new THREE.Mesh(otherSphereGeometry, sphereMaterial)
     sphere2.material.side = THREE.BackSide;
     scene.add(sphere2)
     sphere2.position.set(-740, 0, -1000)
 
     var outerSphereGeometry = new THREE.SphereGeometry(1100, 64, 64, 0, Math.PI, 0, Math.PI / 2)
-    var outerSphere = new Physijs.SphereMesh(outerSphereGeometry, sphereMaterial)
+    var outerSphere = new THREE.Mesh(outerSphereGeometry, sphereMaterial)
     scene.add(outerSphere)
     outerSphere.position.set(-740, 0, 1350)
     var outerSphereGeometry2 = new THREE.SphereGeometry(1100, 64, 64, Math.PI, Math.PI, 0, Math.PI / 2)
