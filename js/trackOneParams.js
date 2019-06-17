a1b4b699b39a6d7774d62fa2a491285f6c844c48
Alexandro <alexandro.strauss@gmail.com>
2019-06-16 22:30:42 -0400

    delete unnecessary files

--- js/trackOneParams.js
+++
@@ -1,9 +0,0 @@
-function trackOneParams(xPosition, zPosition) {
-    if (xPosition >= 225 || xPosition <= -1700) {
-        return true
-    } else if (xPosition >= -1125 && xPosition <= -250 && zPosition >= -800 && zPosition <= 1000) {
-        return true
-    } 
-
-    return false
-}
